8c442501161eed5d8d388afd59cf90648f6fee01
GiovanniCadoni <giocadoni19@gmail.com>
2024-02-02 23:26:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import AppMain from './components/AppMain.vue';
+
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    components: { AppMain },
+  }
+</script>
+
+<template>
+  <h1>
+    Hello world!
+  </h1>
+  <AppMain />
+</template>
+
+<style lang="scss">
+  @use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        data() {
+            return{
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <h2>
+        Main Component
+    </h2>
+</template>
+
+<style lang="scss" scoped>
+</style>
